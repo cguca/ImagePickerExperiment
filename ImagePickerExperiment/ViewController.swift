0d27f99ae661560d87a8d479f4782dc4f499881b
Cary Guca <cary.guca@gmail.com>
2021-01-11 16:06:42 -0600

    Initial commit

--- ImagePickerExperiment/ViewController.swift
+++ ImagePickerExperiment/ViewController.swift
@@ -14,6 +14,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+   
+    @IBAction func experiment(_ sender: Any) {
+        let nextController = UIImagePickerController()
+        present(nextController, animated: true, completion: nil)
+    }
+    
+    @IBAction func experimentAlert(_ sender: Any) {
+        let controller = UIAlertController()
+        controller.title = "Test Alert"
+        controller.message = "This is a test alert"
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {action in self.dismiss(animated: true, completion: nil)})
+        controller.addAction(okAction)
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func experimentActivity(_ sender: Any) {
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.present(controller, animated: true, completion: nil)
+    }
+    
 }
 
